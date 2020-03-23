dc3d4ef00403c213f27a85239ffe01e33ea3b23b
Victor Tran <60512497+VictorTran-rc@users.noreply.github.com>
2020-03-23 02:54:32 -0400

    Add files via upload

--- public/main.js
+++ public/main.js
@@ -1,5 +1,5 @@
 var thumbUp = document.getElementsByClassName("fa-thumbs-up");
-var thumbDown = document.getElementsByClassName("fa-thumb-down")
+var thumbDown = document.getElementsByClassName("fa-thumbs-down");
 var trash = document.getElementsByClassName("fa-trash");
 
 Array.from(thumbUp).forEach(function(element) {
@@ -7,7 +7,7 @@ Array.from(thumbUp).forEach(function(element) {
         const name = this.parentNode.parentNode.childNodes[1].innerText
         const msg = this.parentNode.parentNode.childNodes[3].innerText
         const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
-        fetch('thumbUp', {
+        fetch('messages', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
@@ -31,6 +31,7 @@ Array.from(thumbDown).forEach(function(element) {
         const name = this.parentNode.parentNode.childNodes[1].innerText
         const msg = this.parentNode.parentNode.childNodes[3].innerText
         const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+        console.log(thumbDown)
         fetch('thumbDown', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
@@ -44,7 +45,7 @@ Array.from(thumbDown).forEach(function(element) {
           if (response.ok) return response.json()
         })
         .then(data => {
-          // console.log(data)
+          console.log(data)
           window.location.reload(true)
         })
       });
@@ -54,8 +55,7 @@ Array.from(trash).forEach(function(element) {
       element.addEventListener('click', function(){
         const name = this.parentNode.parentNode.childNodes[1].innerText
         const msg = this.parentNode.parentNode.childNodes[3].innerText
-        const thumbDown = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
-        fetch('thumpDown', {
+        fetch('messages', {
           method: 'delete',
           headers: {
             'Content-Type': 'application/json'
